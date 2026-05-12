7c8f465eab0a9996753c26b13c8208e737a5bd74
unknown <acoder160@gmail.com>
2026-05-12 11:09:01 +0200

    Añadido el servicio IA de moderacion, he puesto una cascada de 5 modelos por si alguna falla o no tendre tokens, ahora ia filtraria titulo, descripcion y comentarios de spots

--- skatemap/src/main/java/skatemap/SkatemapApplication.java
+++ skatemap/src/main/java/skatemap/SkatemapApplication.java
@@ -1,5 +1,6 @@
 package skatemap;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SkatemapApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SkatemapApplication.class, args);
+        // Cargamos .env
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(SkatemapApplication.class, args);
 	}
 }
